a48d821bfbca7dafcc395cf20aafcbf1b00cc334
Daniel <danielrojasa15@gmail.com>
2023-07-31 17:54:10 -0400

    finished

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -3,35 +3,130 @@
 #include <stdio.h>
 #include <string.h>
 
+//arr = {1,2,3,4,5,6,7,8,9,10,11}; size 9
 
+//mid = 4;
+//n1 = 4 - 0 + 1 = 5
+//n2 = 9 - 4 = 5
 int extraMemoryAllocated;
 
+
+void merge(int arr[], int l, int mid, int r)
+{
+    int a = 0, b = 0;
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+
+    int left[n1],right[n2];
+
+    for (int i = 0; i < n1;i++){
+        left[i] = arr[l + i];
+    }
+
+    for (int j = 0; j < n2;j++){
+        right[j] = arr[mid + 1 + j];
+    }
+
+    int c = l;
+    while(a < n1 && b < n2){
+        if(left[a] <= right[b]){
+            arr[c] = left[a];
+            a++;
+        }else{
+            arr[c] = right[b];
+            b++;
+        }
+        c++;
+    }
+
+    while(a < n1){
+        arr[c] = left[a];
+        a++;
+        c++;
+    }
+    while(b < n2){
+        arr[c] = right[b];
+        b++;
+        c++;
+    }
+}
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-    printf("Hello, world!");
+    if(l < r){
+        int mid = l + (r - l) / 2;
+
+
+        mergeSort(pData,l,mid);
+        mergeSort(pData,mid+1,r);
+
+        merge(pData,l,mid,r);
+    }
+
 }
+
+
 
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
-	
+    for (int step = 1; step < n; step++) {
+        int key = pData[step];
+        int j = step - 1;
+        while (key < pData[j] && j >= 0) {
+           pData[j + 1] = pData[j];
+            --j;
+        }
+        pData[j + 1] = key;
+    }
+}
+
+void swap(int *x,int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
-	
+    // loop to access each array element
+    for (int step = 0; step < n - 1; step++) {
+
+        // loop to compare array elements
+        for (int i = 0; i < n - step - 1; i++) {
+
+            // compare two adjacent elements
+            // change > to < to sort in descending order
+            if (pData[i] > pData[i + 1]) {
+
+                // swapping occurs if elements
+                // are not in the intended order
+
+                swap(&pData[i],&pData[i+1]);
+
+            }
+        }
+    }
 }
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
-	
+    for (int step = 0; step < n - 1; step++) {
+        int min = step;
+        for (int i = step + 1; i < n; i++) {
+
+
+            if (pData[i] < pData[min])
+                min = i;
+        }
+        swap(&pData[min], &pData[step]);
+    }
 }
 
 // parses input file to an integer array
@@ -69,12 +164,26 @@ void printArray(int pData[], int dataSz)
 	printf("\n\n");
 }
 
+
 int main(void)
 {
+/*
+    int arr[] = {24,15,4,9,20,57,19,1102,4753,234,12,456,453,};
+
+    int size = sizeof(arr) / sizeof (arr[0]);
+    mergeSort(arr,0,size-1);
+    //selectionSort(arr,12);
+    //bubbleSort(arr,12);
+    //insertionSort(arr,12);
+
+    printArr(arr,12);
+*/
+
+
 	clock_t start, end;
 	int i;
     double cpu_time_used;
-	char* fileNames[] = {"input1.txt", "input2.txt", "input3.txt"};
+	char* fileNames[] = {"C:\\Users\\blueg\\CLionProjects\\assignment_4\\compareSortingAlgorithms\\input1.txt", "C:\\Users\\blueg\\CLionProjects\\assignment_4\\compareSortingAlgorithms\\input2.txt", "C:\\Users\\blueg\\CLionProjects\\assignment_4\\compareSortingAlgorithms\\input3.txt"};
 	
 	for (i=0;i<3;++i)
 	{
@@ -137,5 +246,5 @@ int main(void)
 		free(pDataCopy);
 		free(pDataSrc);
 	}
-	
+
 }
